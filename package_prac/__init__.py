7b944685f932b949176ef67264b110614a94411a
Soude90 <soumronaldo09@gmail.com>
2021-07-12 15:30:18 +0530

    new commit

--- package_prac/__init__.py
+++ package_prac/__init__.py
@@ -3,82 +3,82 @@ from package_prac import utils
 __version__ = '0.10.3'
 
 def get_wordcounts(x):
-	return utils._get_wordcounts(x)
+	return utils.get_wordcounts1(x)
 
 def get_charcounts(x):
-	return utils._get_charcounts(x)
+	return utils.get_charcounts1(x)
 
 def get_avg_wordlength(x):
-	return utils._get_avg_wordlength(x)
+	return utils.get_avg_wordlength1(x)
 
 def get_stopwords_counts(x):
-	return utils._get_stopwords_counts(x)
+	return utils.get_stopwords_counts1(x)
 
 def get_hashtag_counts(x):
-	return utils._get_hashtag_counts(x)
+	return utils.get_hashtag_counts1(x)
 
 def get_mentions_counts(x):
-	return utils._get_mentions_counts(x)
+	return utils.get_mentions_counts1(x)
 
 def get_digit_counts(x):
-	return utils._get_digit_counts(x)
+	return utils.get_digit_counts1(x)
 
 def get_uppercase_counts(x):
-	return utils._get_uppercase_counts(x)
+	return utils.get_uppercase_counts1(x)
 
 def cont_exp(x):
-	return utils._cont_exp(x)
+	return utils.cont_exp1(x)
 
 def get_emails(x):
-	return utils._get_emails(x)
+	return utils.get_emails1(x)
 
 def remove_emails(x):
-	return utils._remove_emails(x)
+	return utils.remove_emails1(x)
 
 def get_urls():
-	return utils._get_urls(x)
+	return utils.get_urls1(x)
 
 def remove_urls(x):
-	return utils._remove_urls(x)
+	return utils.remove_urls1(x)
 
 def remove_rt(x):
-	return utils._remove_rt(x)
+	return utils.remove_rt1(x)
 
 def remove_special_chars(x):
-	return utils._remove_special_chars(x)
+	return utils.remove_special_chars1(x)
 
 def remove_html_tags(x):
-	return utils._remove_html_tags(x)
+	return utils.remove_html_tags1(x)
 
 def remove_accented_chars(x):
-	return utils._remove_accented_chars(x)
+	return utils.remove_accented_chars1(x)
 
 def remove_stopwords(x):
-	return utils._remove_stopwords(x)
+	return utils.remove_stopwords1(x)
 
 def make_base(x):
-	return utils._make_base(x)
+	return utils.make_base1(x)
 
 def get_value_counts(df, col):
-	return utils._get_value_counts(df, col)
+	return utils.get_value_counts1(df, col)
 
 def get_word_freqs(df, col):
-	return utils._get_value_counts(df, col)
+	return utils.get_value_counts1(df, col)
 
 def remove_common_words(x, freq, n=20):
-	return utils._remove_common_words(x, freq, n)
+	return utils.remove_common_words1(x, freq, n)
 
 def remove_rarewords(x, freq, n=20):
-	return utils._remove_rarewords(x, freq, n)
+	return utils.remove_rarewords1(x, freq, n)
 
 def spelling_correction(x):
-	return utils._spelling_correction(x)
+	return utils.spelling_correction1(x)
 
 def remove_dups_char(x):
-	return utils._remove_dups_char(x)
+	return utils.remove_dups_char1(x)
 
 def get_basic_features(df):
-	return utils._get_basic_features(df)
+	return utils.get_basic_features1(df)
 
 def get_ngram(df, col, ngram_range):
-	return utils._get_ngram(df, col, ngram_range)
+	return utils.get_ngram1(df, col, ngram_range)
--- package_prac/utils.py
+++ package_prac/utils.py
@@ -20,39 +20,39 @@ path = os.path.dirname(os.path.abspath(__file__))
 abbreviations_path = os.path.join(path, 'data','abbreviations_wordlist.json')
 
 
-def _get_wordcounts(x):
+def get_wordcounts1(x):
 	length = len(str(x).split())
 	return length
 
-def _get_charcounts(x):
+def get_charcounts1(x):
 	s = x.split()
 	x = ''.join(s)
 	return len(x)
 
-def _get_avg_wordlength(x):
+def get_avg_wordlength1(x):
 	count = _get_charcounts(x)/_get_wordcounts(x)
 	return count
 
-def _get_stopwords_counts(x):
+def get_stopwords_counts1(x):
 	l = len([t for t in x.split() if t in stopwords])
 	return l
 
-def _get_hashtag_counts(x):
+def get_hashtag_counts1(x):
 	l = len([t for t in x.split() if t.startswith('#')])
 	return l
 
-def _get_mentions_counts(x):
+def get_mentions_counts1(x):
 	l = len([t for t in x.split() if t.startswith('@')])
 	return l
 
-def _get_digit_counts(x):
+def get_digit_counts1(x):
 	digits = re.findall(r'[0-9,.]+', x)
 	return len(digits)
 
-def _get_uppercase_counts(x):
+def get_uppercase_counts1(x):
 	return len([t for t in x.split() if t.isupper()])
 
-def _cont_exp(x):
+def cont_exp1(x):
 	abbreviations = json.load(open(abbreviations_path))
 
 	if type(x) is str:
@@ -66,44 +66,44 @@ def _cont_exp(x):
 		return x
 
 
-def _get_emails(x):
+def get_emails1(x):
 	emails = re.findall(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+\b)', x)
 	counts = len(emails)
 
 	return counts, emails
 
 
-def _remove_emails(x):
+def remove_emails1(x):
 	return re.sub(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+)',"", x)
 
-def _get_urls(x):
+def get_urls1(x):
 	urls = re.findall(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', x)
 	counts = len(urls)
 
 	return counts, urls
 
-def _remove_urls(x):
+def remove_urls1(x):
 	return re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '' , x)
 
-def _remove_rt(x):
+def remove_rt1(x):
 	return re.sub(r'\brt\b', '', x).strip()
 
-def _remove_special_chars(x):
+def remove_special_chars1(x):
 	x = re.sub(r'[^\w ]+', "", x)
 	x = ' '.join(x.split())
 	return x
 
-def _remove_html_tags(x):
+def remove_html_tags1(x):
 	return BeautifulSoup(x, 'lxml').get_text().strip()
 
-def _remove_accented_chars(x):
+def remove_accented_chars1(x):
 	x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore')
 	return x
 
-def _remove_stopwords(x):
+def remove_stopwords1(x):
 	return ' '.join([t for t in x.split() if t not in stopwords])	
 
-def _make_base(x):
+def make_base1(x):
 	x = str(x)
 	x_list = []
 	doc = nlp(x)
@@ -116,31 +116,31 @@ def _make_base(x):
 		x_list.append(lemma)
 	return ' '.join(x_list)
 
-def _get_value_counts(df, col):
+def get_value_counts1(df, col):
 	text = ' '.join(df[col])
 	text = text.split()
 	freq = pd.Series(text).value_counts()
 	return freq
 
-def _remove_common_words(x, freq, n=20):
+def remove_common_words1(x, freq, n=20):
 	fn = freq[:n]
 	x = ' '.join([t for t in x.split() if t not in fn])
 	return x
 
-def _remove_rarewords(x, freq, n=20):
+def remove_rarewords1(x, freq, n=20):
 	fn = freq.tail(n)
 	x = ' '.join([t for t in x.split() if t not in fn])
 	return x
 
-def _remove_dups_char(x):
+def remove_dups_char1(x):
 	x = re.sub("(.)\\1{2,}", "\\1", x)
 	return x
 
-def _spelling_correction(x):
+def spelling_correction1(x):
 	x = TextBlob(x).correct()
 	return x
 
-def _get_basic_features(df):
+def get_basic_features1(df):
 	if type(df) == pd.core.frame.DataFrame:
 		df['char_counts'] = df['text'].apply(lambda x: _get_charcounts(x))
 		df['word_counts'] = df['text'].apply(lambda x: _get_wordcounts(x))
@@ -156,7 +156,7 @@ def _get_basic_features(df):
 	return df
 
 
-def _get_ngram(df, col, ngram_range):
+def get_ngram1(df, col, ngram_range):
 	vectorizer = CountVectorizer(ngram_range=(ngram_range, ngram_range))
 	vectorizer.fit_transform(df[col])
 	ngram = vectorizer.vocabulary_
